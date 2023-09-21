398e8b10592387653e7d3833f2eeb6489ca9fc27
Source Academy User <No public email provided>
2023-09-21 16:05:44 +0800

    Changes made from Source Academy

--- Reflection R6.js
+++ Reflection R6.js
@@ -35,3 +35,62 @@ insertion_sort_cmp(xs, (x, y) => (x % 2 === 0 && y % 2 === 0 && x <= y) ||
                                  (x % 2 === 0 && y % 2 === 1) ||
                                  (x % 2 === 1 && y % 2 === 1 && x >= y));
 // Result: list(2, 4, 6, 6, 8, 9, 7, 5, 3, 1)
+
+
+
+//Q2.Consider the partial implementation of merge_sort shown in class:
+// half, rounded downwards
+function middle(n) {
+return math_floor(n / 2);
+}
+// put the first n elements of xs into a list
+function take(xs, n) {
+    return n === 0 ? null : pair(head(xs), take(tail(xs), n - 1));
+}
+// drop the first n elements from the list and return the rest
+function drop(xs, n) {
+    return n === 0 ? xs : tail(drop(xs, n - 1));
+}
+
+// merge two sorted lists into one sorted list
+function merge(xs, ys) {
+    if (is_null(xs)) {
+        return ys;
+    } else if (is_null(ys)) {
+        return xs;
+    } else {
+        const x = head(xs);
+        const y = head(ys);
+        return x < y
+               ? pair(x, merge(tail(xs), ys))
+               : pair(y, merge(xs, tail(ys)));
+    }
+}
+
+// a list with more than one element is sorted
+// by splitting it into two lists of (almost) equal
+// length, sorting the halves and then merging them
+// together
+function merge_sort(xs) {
+    if (is_null(xs) || is_null(tail(xs))) {
+        return xs;
+    } else {
+        const mid = middle(length(xs));
+        return merge(merge_sort(take(xs, mid)),
+                     merge_sort(drop(xs, mid)));
+    }
+}
+
+// Test
+merge_sort(list(7, 6, 3, 8, 4, 6, 5, 9, 8, 3, 1, 5, 2));
+
+//(a) Θ(n)
+
+//(b) Θ(n)
+
+
+
+
+
+
+
