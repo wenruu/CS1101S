145e9ed7eca3819060541f9417a1492652edd357
Source Academy User <No public email provided>
2023-09-21 15:58:30 +0800

    Changes made from Source Academy

--- Reflection R6.js
+++ Reflection R6.js
@@ -19,15 +19,15 @@ function insertion_sort_cmp(xs, cmp) {
 const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2, 7);
 
 // (a)
-//insertion_sort_cmp(xs, (x, y) => x < y);
+insertion_sort_cmp(xs, (x, y) => x < y);
 // Result: list(1, 2, 3, 4, 5, 6, 6, 7, 8, 9)
 
 // (b)
-//insertion_sort_cmp(xs, ((x, y) => x > y));
+insertion_sort_cmp(xs, ((x, y) => x > y));
 // Result: list(9, 8, 7, 6, 6, 5, 4, 3, 2, 1)
 
 // (c)
-//insertion_sort_cmp(xs, (x, y) => is_null(y));
+insertion_sort_cmp(xs, (x, y) => is_null(y));
 // Result: list(7, 2, 4, 6, 9, 1, 5, 8, 3, 6)
 
 // (d)
