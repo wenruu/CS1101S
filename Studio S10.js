0188099dac58b2901321d9384bdd4cad9a5e4fbf
Source Academy User <No public email provided>
2023-10-23 17:26:26 +0800

    Changes made from Source Academy

--- Studio S10.js
+++ Studio S10.js
@@ -26,4 +26,25 @@ const LL = list(3, 5, 2, 4, 1);
 bubblesort_list(LL);
 LL; // should show [1, [2, [3, [4, [5, null]]]]]
 
-//Q3
+//Q3a
+// yes b/c same as n choose k
+
+//Q3b
+function mcc(n, k) {
+    // Your solution here.
+    if (n < 0 || k === 0) {
+        return 0;
+    } else if (n === 0) {
+        return 1;
+    } else if (read(n, k) !== undefined) {
+        return read(n,k);
+    } else {
+        const val = mcc(n, k - 1) + mcc(n - first_denomination(k),
+                                        k);
+        write(n, k, val);
+        return val;
+    }
+}
+
+//Q3c
+//n, n?
