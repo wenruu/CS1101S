9da51e0388c5ea5fa24d0cc2cc5b9510f9d7eec4
Source Academy User <No public email provided>
2023-11-21 14:34:11 +0800

    Changes made from Source Academy

--- 1718_final.js
+++ 1718_final.js
@@ -1,3 +1,24 @@
+//1B:
+function similar(tn1, tn2) {
+    return is_null(tn1) && is_null(tn2)
+           ? true
+           : is_number(tn1) && is_number(tn2)
+           ? math_abs(tn1 - tn2) <= 1
+           : is_null(tn1) || is_null(tn2)
+           ? false
+           : is_list(tn1) && is_list(tn2)
+           ? similar(head(tn1), head(tn2)) && similar(tail(tn1), tail(tn2))
+           : false;
+}
+//testing:
+// similar(list(4, list(5,6)), 
+//         list(4,null,list(5,6))); // false (not same structure)
+// similar(list(4, null, list(5,6)),
+//         list(5, null, list(4,7))); // true
+// similar(list(4, list(5,6)),
+//         list(5, list(3,7))); // false (too big difference)
+
+//1C.
 function differences(tn1, tn2) {
     return is_null(tn1) && is_null(tn2) ? 0
            : is_number(tn1) && is_number(tn2)
@@ -7,4 +28,35 @@ function differences(tn1, tn2) {
                 differences(tail(tn1), tail(tn2))
            : 1;
 }
+//testing:
+// differences(list(4, null, list(4,6), 8),
+// list(5, null, list(4,7), 8)); // returns 2
+// differences(list(4,5,6,7),
+// list(4,5,6,7)); // returns 0
+
+//1D.
+function map_tree(f, tree) {
+    return map(sub_tree => ! is_list(sub_tree)
+                           ? f(sub_tree)
+                           : map_tree(f, sub_tree),
+               tree
+               );
+}
+function increment(tn) {
+    return map_tree(x => x + 1, tn);
+}
+//increment(list(4, null, list(4,6), 8));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
